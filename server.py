fe7b4de045f1747d07f32a4d5e925ddb5967595c
Alyson Rehm <ALY.REHM@gmail.com>
2022-10-29 10:53:13 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, request  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+from users import User
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    return render_template('users.html', users=User.get_all_users())
+
+@app.route('/create_user')
+def create():
+    return render_template('create.html')
+
+@app.route('/user/create', methods=['POST'])
+def add():
+    User.new_user(request.form)
+    return redirect('/users')
+
+
+# route for editing user
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edit_user.html",user=User.get_one(data))
+
+
+#ROUTE TO RETURN TO TO EDIT USERS 
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+
+#ROUTE FOR DELETING USER   
+# note to self - be aware that 'delete' can be a reserved word in some languages 
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={ 
+        "id":id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
+
+#ROUTE TO RETURN TO TO SHOW USERS 
+# similiar to the edit, but this time we just want to view 
+@app.route('/user/view/<int:id>')
+def view(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("view_user.html",user=User.get_one(data))
+
+
+
+#ALWAYS INCLUDE!! 
+if __name__=="__main__":
+    app.run(debug=True) 
